db375e552e39b4676bf2c1630e44bb7b21160656
Reavlol <Brandongriffiths1000@gmail.com>
2025-03-08 17:26:32 +0000

    uppdate for frame rate cap and added circles

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  # Game loop
         screen.fill((0, 0, 0))  # Fill screen with black
         pygame.display.flip()  # Refresh screen
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
